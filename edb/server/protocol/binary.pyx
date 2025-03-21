a08c6e3124d4fde046ae3b6b6de0c6a54c294aca
Elvis Pranskevichus <elvis@edgedb.com>
2025-03-21 16:06:22 -0700

    Use `read committed` when restoring dumps
    There's no point in running in `serializable` when restoring things
    because we currently restore into empty relations always.

--- edb/server/protocol/binary.pyx
+++ edb/server/protocol/binary.pyx
@@ -1628,7 +1628,7 @@ cdef class EdgeConnection(frontend.FrontendConnection):
         async with self._with_dump_restore_pgcon() as pgcon:
             _dbview.decode_state(sertypes.NULL_TYPE_ID.bytes, b'')
             await self._execute_utility_stmt(
-                'START TRANSACTION ISOLATION SERIALIZABLE',
+                'START TRANSACTION ISOLATION READ COMMITTED',
                 pgcon,
             )
 
