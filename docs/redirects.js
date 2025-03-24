ce7558b2a519cf5ad00770b28abaca7d5c24052c
James Clarke <james@edgedb.com>
2025-03-24 21:30:38 +0000

    Add placeholder redirects.js

---
+++ docs/redirects.js
@@ -0,0 +1,9 @@
+// See https://nextjs.org/docs/app/api-reference/config/next-config-js/redirects
+module.exports = [
+  {
+    // REMOVE ME
+    source: "/test",
+    destination: "/resources/guides",
+    permanent: false,
+  },
+];
