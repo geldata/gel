273e9837d9895c3d97fc31f02d3837149f0d4208
Aljaž Mur Eržen <aljaz@edgedb.com>
2025-03-28 10:03:28 +0100

    rust apply new clippy lints

--- edb/edgeql-parser/src/helpers/bytes.rs
+++ edb/edgeql-parser/src/helpers/bytes.rs
@@ -7,7 +7,7 @@ pub fn unquote_bytes(value: &str) -> Result<Vec<u8>, String> {
         "br" | "rb" => Ok(value[3..value.len() - 1].as_bytes().to_vec()),
         "b" => Ok(unquote_bytes_inner(&value[2..value.len() - 1])?),
         _ => {
-            return Err(format_args!(
+            Err(format_args!(
                 "prefix {:?} is not allowed for bytes, allowed: `b`, `rb`",
                 prefix
             )
