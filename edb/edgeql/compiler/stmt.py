4a3d7db686da6f94d8faee190a414a27175293c5
Aljaž Mur Eržen <aljaz@edgedb.com>
2025-03-28 11:46:43 +0100

    Expand span of sets to include applied shape

--- edb/edgeql/compiler/stmt.py
+++ edb/edgeql/compiler/stmt.py
@@ -30,6 +30,7 @@ from typing import (
 
 from collections import defaultdict
 import textwrap
+import itertools
 
 from edb import errors
 from edb.common import ast
@@ -1583,6 +1584,15 @@ def compile_query_subject(
 
     if (shape is not None or view_scls is not None) and len(set.path_id) == 1:
         ctx.class_view_overrides[set.path_id.target.id] = set_stype
+
+    if shape:
+        # make sure that an applied shape expands the span of the set
+        set.span = edb_span.merge_spans(
+            itertools.chain(
+                (s.span for s in [set] if s.span),
+                (el.span for el in shape if el.span)
+            )
+        )
 
     return set
 
