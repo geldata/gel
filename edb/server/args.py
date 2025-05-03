b1b6979a56b44fe8b60767199ca36770d3f4c190
Fantix King <fantix.king@gmail.com>
2025-05-02 21:37:06 -0400

    server: add more compiler pool metrics (#8655)

--- edb/server/args.py
+++ edb/server/args.py
@@ -1158,8 +1158,10 @@ server_options = typeutils.chain_decorators([
     click.option(
         '--compiler-worker-max-rss',
         type=int,
-        help='Maximum allowed RSS (in KiB) per compiler worker process. Any '
-             'worker exceeding this limit will be terminated and recreated.',
+        help='Maximum allowed RSS (in bytes) per compiler worker process. Any '
+             'worker exceeding this limit will be terminated and recreated. '
+             'Each worker is free from this limit in its first 20-30 hours '
+             'after spawn to avoid infinite restarts or a thundering herd.',
     ),
 ])
 
@@ -1205,8 +1207,10 @@ compiler_options = typeutils.chain_decorators([
     click.option(
         '--worker-max-rss',
         type=int,
-        help='Maximum allowed RSS (in KiB) per worker process. Any worker '
-             'exceeding this limit will be terminated and recreated.',
+        help='Maximum allowed RSS (in bytes) per worker process. Any worker '
+             'exceeding this limit will be terminated and recreated. '
+             'Each worker is free from this limit in its first 20-30 hours '
+             'after spawn to avoid infinite restarts or a thundering herd.',
     ),
 ])
 
