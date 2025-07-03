d0fae54bcf344b92ff0a2b0d602ee7353e683b77
dnwpark <dnwpark@protonmail.com>
2025-07-03 14:13:30 -0700

    Allow extensions to have inner modules.

--- edb/schema/extensions.py
+++ edb/schema/extensions.py
@@ -39,6 +39,7 @@ from . import annos as s_anno
 from . import casts as s_casts
 from . import delta as sd
 from . import indexes as s_indexes
+from . import modules as s_mod
 from . import name as sn
 from . import objects as so
 from . import schema as s_schema
@@ -869,8 +870,14 @@ class DeleteExtension(
 
         def _name_in_mod(name: sn.Name) -> bool:
             return (
-                (isinstance(name, sn.QualName) and name.module == module)
-                or name == module_name
+                (
+                    isinstance(name, sn.QualName)
+                    and name.module.startswith(module_name)
+                )
+                or (
+                    isinstance(name, sn.UnqualName)
+                    and name.name.startswith(module_name)
+                )
             )
 
         # Clean up the casts separately because we can't keep them in
@@ -911,11 +918,14 @@ class DeleteExtension(
 
         # We handle deleting the module contents in a heavy-handed way:
         # do a schema diff.
+        module_names = [
+            m.get_name(schema)
+            for m in schema.get_modules()
+            if _name_in_mod(m.get_name(schema))
+        ]
         delta = s_ddl.delta_schemas(
             schema, schema,
-            included_modules=[
-                sn.UnqualName(module),
-            ],
+            included_modules=module_names,
             schema_b_filters=[filt],
             include_extensions=True,
             linearize_delta=True,
