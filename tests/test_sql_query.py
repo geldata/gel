d17a078f2e391cfc531546e359f5713b69268939
Michael J. Sullivan <sully@msully.net>
2025-08-19 13:26:52 -0700

    sql: Fix COLLATE clauses that require quotation (#8960)
    We were failing to quote the name of the collation.
    
    ---------
    
    Co-authored-by: Aljaž Mur Eržen <aljaz@erzen.si>

--- tests/test_sql_query.py
+++ tests/test_sql_query.py
@@ -1205,6 +1205,18 @@ class TestSQLQuery(tb.SQLQueryTestCase):
             ]
         )
 
+    async def test_sql_query_59(self):
+        await self.squery_values(
+            '''
+            SELECT 'lol' COLLATE "C";
+            '''
+        )
+        await self.squery_values(
+            '''
+            SELECT 'lol' COLLATE pg_catalog.default;
+            '''
+        )
+
     async def test_sql_query_introspection_00(self):
         dbname = self.con.dbname
         res = await self.squery_values(
