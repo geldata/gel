1889db11670fd043ca0b78b78b23042a7ff7ec9d
Michael J. Sullivan <sully@msully.net>
2025-08-20 12:41:26 -0700

    drop the repair, it does not work

--- edb/pgsql/patches.py
+++ edb/pgsql/patches.py
@@ -67,7 +67,4 @@ The current kinds are:
 """
 PATCHES: list[tuple[str, str]] = [
     # 7.0b2 or 7.0rc1
-    # In case we are upgrading from a 7.0b1 that was IPUd to, this
-    # should fix up the missing permissions.
-    ('repair', ''),
 ]
