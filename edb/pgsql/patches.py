6b60a92109b947187eee931d413a3dbfad5a61f6
Michael J. Sullivan <sully@msully.net>
2025-09-23 11:31:53 -0700

    Fix modifying and dropping inherited AI indexes (#9041)
    The bug is that the index's `kwargs` aren't inherited in subtypes
    (which is bad), though the kwargs are still used in determining the
    internal name of the index. But worse, subsubtypes have no exposure to
    these kwargs, and so the kwargs aren't used in determining the name,
    either, which prevents it from getting deleted properly.
    
    Make sure to include kwargs. Added some documentation to try to avoid
    missing this kind of thing.
    
    Might be worth reviewing by commits -- I also had to commit some hacks
    in order to get `schema_repair` to work.
    
    I tested that manually, and will try to test it automatically when I
    backport this.
    
    Fixes #9033.

--- edb/pgsql/patches.py
+++ edb/pgsql/patches.py
@@ -157,11 +157,14 @@ PATCHES: list[tuple[str, str]] = [
         };
     };
 '''),
-    # 7.0rc3 or 7.0 final??
+    # 7.0rc3
 
     # For #9028.
     # N.B: We don't need to do any actual in place patching, since it
     # just adjusts the compat version numbers. If it is installed,
     # then we're already fine.
     ('ext-pkg', 'pgvector'),
+    # For #9041, unfortunately -- this may actually drop and recreate
+    # ai indexes in the database.
+    ('repair', ''),
 ]
