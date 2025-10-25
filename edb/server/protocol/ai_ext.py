1d087f5d16f03bc77ceb0b32e2fd899defa66960
fzowl <zoltan@voyageai.com>
2025-10-25 14:05:13 +0200

    Adding Voyage 3.5 models

--- edb/server/protocol/ai_ext.py
+++ edb/server/protocol/ai_ext.py
@@ -111,6 +111,7 @@ class BadRequestError(AIExtError):
 class ApiStyle(s_enum.StrEnum):
     OpenAI = 'OpenAI'
     Anthropic = 'Anthropic'
+    VoyageAI = 'VoyageAI'
     Ollama = 'Ollama'
 
 
@@ -1267,6 +1268,10 @@ async def _generate_embeddings(
         result = await _generate_openai_embeddings(
             provider, model_name, inputs, shortening, user, http_client
         )
+    elif provider.api_style == ApiStyle.VoyageAI:
+        return await _generate_voyageai_embeddings(
+            provider, model_name, inputs, http_client
+        )
     elif provider.api_style == ApiStyle.Ollama:
         result = await _generate_ollama_embeddings(
             provider, model_name, inputs, shortening, http_client
@@ -1333,6 +1338,50 @@ async def _generate_openai_embeddings(
     return EmbeddingsResult(
         data=(error if error else EmbeddingsData(result.bytes())),
         limits=_read_openai_limits(result),
+    )
+
+
+async def _generate_voyageai_embeddings(
+    provider: ProviderConfig,
+    model_name: str,
+    inputs: list[str],
+    http_client: http.HttpClient,
+) -> EmbeddingsResult:
+
+    headers = {
+        "Authorization": f"Bearer {provider.secret}",
+    }
+    client = http_client.with_context(
+        headers=headers,
+        base_url=provider.api_url,
+    )
+
+    params: dict[str, Any] = {
+        "input": inputs,
+        "model": model_name,
+    }
+
+    result = await client.post(
+        "/embeddings",
+        json=params,
+    )
+
+    error = None
+    if result.status_code >= 400:
+        error = rs.Error(
+            message=(
+                f"API call to generate embeddings failed with status "
+                f"{result.status_code}: {result.text}"
+            ),
+            retry=(
+                # If the request fails with 429 - too many requests, it can be
+                # retried
+                result.status_code == 429
+            ),
+        )
+
+    return EmbeddingsResult(
+        data=(error if error else EmbeddingsData(result.bytes())),
     )
 
 
